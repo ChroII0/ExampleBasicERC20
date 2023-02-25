5b9d3fdc15665ca736de79788c3a67fa21ea5b79
dhhVo1 <vohuynhhaidang24032001@gmail.com>
2023-02-25 19:00:02 +0700

    fix tokensale demo

--- client/src/components/exampleERC20/tokenSaleDemo/accordions/BalanceOfDemo.tsx
+++ client/src/components/exampleERC20/tokenSaleDemo/accordions/BalanceOfDemo.tsx
@@ -1,10 +1,4 @@
 
-import Image from 'react-bootstrap/Image'
-
-const listGifs = [
-    "https://images6.fanpop.com/image/photos/37800000/Kikyou-InuYasha-egyptprincess7-37801183-500-281.gif",
-]
-
 export const BalanceOfDemo = () => {
     return (
         <>
