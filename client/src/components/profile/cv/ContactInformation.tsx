535c3f7945472750766e571e79bbd5ac80c5f7c6
dhhVo1 <vohuynhhaidang24032001@gmail.com>
2023-02-26 21:23:00 +0700

    set mailto href

--- client/src/components/profile/cv/ContactInformation.tsx
+++ client/src/components/profile/cv/ContactInformation.tsx
@@ -24,7 +24,7 @@ export const ContactInformation = (props: Props) => {
                 <p><strong>Dob:</strong> {props.data.dob}</p>
                 <p><strong>Gender:</strong> {props.data.gender}</p>
                 <p><strong>Phone:</strong> {props.data.phone}</p>
-                <p><strong>Email:</strong> {props.data.email}</p>
+                <p><strong>Email:</strong> <a target="_self" className="text-decoration-none" href={`mailto:${props.data.email}`}>{props.data.email}</a></p>
                 <p><strong>Website:</strong> <a href={props.data.website} target="_blank" rel="noopener noreferrer" className="text-decoration-none">{props.data.website}</a></p>
                 <p><strong>Address:</strong> {props.data.address}</p>
             </Col>
