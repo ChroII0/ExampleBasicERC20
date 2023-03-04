c7b90d0168be78bb239b41dc993a52eeaa158653
dhhVo1 <vohuynhhaidang24032001@gmail.com>
2023-03-04 22:10:57 +0700

    fix the display of the access_log span element in the footer.

--- client/src/views/Footer.tsx
+++ client/src/views/Footer.tsx
@@ -14,7 +14,7 @@ export const Footer = () => {
                 <ul className="list-unstyled d-flex justify-content-between">
                     <li className='flex-grow-1'>
                         <span className='d-block'>Visitor public IP: <strong>{profile.data.Visitor.visitor_ip}</strong></span>
-                        <span>Access log: <strong>{profile.data.Visitor.access_log}</strong></span>
+                        <span className='d-block'>Access log: <strong>{profile.data.Visitor.access_log}</strong></span>
                         <span>Total access log: <strong>{profile.data.Visitor.total_access_log}</strong></span>
                     </li>
                     <li className="ms-3"><a className="text-muted" target="_blank" rel="noopener noreferrer" href={profile.data.SocialMedia[0].facebook}
