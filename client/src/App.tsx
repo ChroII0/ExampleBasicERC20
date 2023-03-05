14f2143c890005ca746b1bfe672acb07bbc02507
dhhVo1 <vohuynhhaidang24032001@gmail.com>
2023-03-05 13:43:15 +0700

    fix check fetch data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 // eslint-disable-next-line
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "./redux/hooks";
 import { selectProfile, setProfile } from "./redux/profile/slides/profileSlide";
@@ -16,16 +16,18 @@ function App() {
   const profile = useAppSelector(selectProfile);
   const dispatch = useAppDispatch();
   const urlApi: string | any = process.env.REACT_APP_URL_API;
+  const [data, setData] = useState({});
   useEffect(() => {
       const fetchProfile = async () => {
           const res = await axios.get(urlApi);
-          dispatch(setProfile(res.data));   
+          dispatch(setProfile(res.data));
+          setData(res.data);
       }
       fetchProfile();
   }, []);  
   return (
     <>
-      {Object.keys(profile.data).length == 8 ?
+      {data.toString()===profile.data.toString() ?
         <>
           <Header />
           <Outlet />
